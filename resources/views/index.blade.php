a52892c599f74324eb6f36149fc87229783ea104
mutemip <paulmutemi2030@gmail.com>
2024-01-27 12:00:59 +0300

    conditional rendering start

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <h1>A blade template!!</h1>
+</div>
+
+<div>
+    @isset($name)
+        Hello, {{$name}}! How are you?
+    @endisset
+</div>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return "Hello there!!";
+    return View("index", 
+    // ["name"=>"Mutemi"]
+    );
 });
 Route::get("/hello", function () {
     return "This is hello page.";
