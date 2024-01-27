6c9561450ae9a44209eea3289d0cc46b33d5e4e5
mutemip <paulmutemi2030@gmail.com>
2024-01-27 13:56:35 +0300

    @forech && @forelse

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,18 @@
 <div>
-    <h1>A blade template!!</h1>
+    <h1>List of all tasks!</h1>
 </div>
 
 <div>
-    @isset($name)
-        Hello, {{$name}}! How are you?
-    @endisset
+    <!-- @if (count($tasks))
+        @foreach ($tasks as $task)
+            <div>{{ $task -> title}}</div>
+        @endforeach  
+    @else
+        <div>There are no tasks yet.</div>
+    @endif -->
+    @forelse ($tasks as $task)
+        <div>{{$task->title}}</div>
+    @empty
+        <div>No tasks</div>
+    @endforelse
 </div>
